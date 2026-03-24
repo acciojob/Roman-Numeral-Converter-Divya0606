07c380ab10a545e24ca478c219fb07da3d1400ec
Divya Agarwal <2021pcecsdivya049@poornima.org>
2026-03-24 21:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,13 +8,20 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-
+	let res='';
+	for(let[symbol,val] of obj){
+		while(num>=val){
+			res+=symbol;
+			num-=val
+		}
+	}
+return res;
   //your code here
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(36));
 
 
 
